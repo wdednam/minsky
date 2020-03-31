02c522cd43b3349c19d6d8fb6f3f8eed81fa90f6
wdednam <wdednam@gmail.com>
2020-03-31 23:57:45 +0200

    update develop branch

--- model/CSVDialog.cc
+++ model/CSVDialog.cc
@@ -28,11 +28,11 @@
 #include <boost/beast/core.hpp>
 #include <boost/beast/http.hpp>
 #include <boost/beast/version.hpp>
-#include <boost/asio/connect.hpp>
-#include <boost/asio/ip/tcp.hpp>
+//#include <boost/asio/connect.hpp>
+//#include <boost/asio/ip/tcp.hpp>
 #include <boost/asio/ssl/error.hpp>
 #include <boost/asio/ssl/stream.hpp>
-//#include <boost/asio.hpp>
+#include <boost/asio.hpp>
 #include <boost/regex.hpp>
 #include <boost/filesystem.hpp>
 #include <cstdlib>

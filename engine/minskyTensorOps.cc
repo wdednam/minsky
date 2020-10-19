e4f409630384ffe0312d388fd1d22cbe0b3f49e5
wdednam <wdednam@gmail.com>
2020-10-19 10:10:26 +0200

    update develop branch

--- engine/minskyTensorOps.cc
+++ engine/minskyTensorOps.cc
@@ -329,9 +329,40 @@ namespace minsky
   {
     std::shared_ptr<ITensor> arg1, arg2;
     void computeTensor() const override {//TODO
-      throw runtime_error("outer product not yet implemented");
+      //throw runtime_error("outer product not yet implemented");
+      size_t m=1, n=1;   
+      if (arg1->rank()>1)
+        for (size_t i=0; i<arg1->rank(); i++)
+          m*=arg1->hypercube().dims()[i];
+          
+      if (arg2->rank()>1)
+        for (size_t i=0; i<arg2->rank(); i++)
+          n*=arg2->hypercube().dims()[i];     
+  	
+      size_t stride=arg2->hypercube().dims()[arg2->rank()-1];	 	 
+      for (size_t i=0; i< m; i++)
+        for (size_t j=0; j< n; j++)
+            for (size_t k=0; k<stride; k++)  
+              {
+                auto v1=m>1? arg1->atHCIndex(k*m+i) : (*arg1)[k];  
+                auto v2=n>1? arg2->atHCIndex(j*stride + k) : (*arg2)[k];  
+                if (!isnan(v1) && !isnan(v2)) cachedResult[i+m*j+k]=v1*v2;
+              }
+    		            
+      if (cachedResult.size()==0) 
+        for (size_t i=0; i<m*n; i++) 
+          cachedResult[i]=nan("");
     }
     Timestamp timestamp() const override {return max(arg1->timestamp(), arg2->timestamp());}
+    void setArguments(const TensorPtr& a1, const TensorPtr& a2) override {
+      arg1=a1; arg2=a2;
+      auto xv1=arg1->hypercube().xvectors, xv2=arg2->hypercube().xvectors;
+      Hypercube hc;
+      hc.xvectors.insert(hc.xvectors.begin(), xv1.begin(), xv1.end());      
+      hc.xvectors.insert(hc.xvectors.begin(), xv2.begin(), xv2.end());        
+      cachedResult.hypercube(move(hc));
+        
+    }      
   };
 
   template <>
